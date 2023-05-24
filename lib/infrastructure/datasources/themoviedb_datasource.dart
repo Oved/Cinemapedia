4d900fdb8bd47d2ed2d5d0f8072cc278633d3e53
Oved Rincon <ogrinconm@ufpso.edu.co>
2023-05-24 11:36:30 -0500

    Se implementan los métodos para realizar las busquedas de las películas

--- lib/infrastructure/datasources/themoviedb_datasource.dart
+++ lib/infrastructure/datasources/themoviedb_datasource.dart
@@ -61,4 +61,11 @@ class MovieDBDataSource extends MoviesDatasource {
     final movie = MovieMapper.movieDetailsToEntity(movieDetails);
     return movie;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    final response =
+        await dio.get('/search/movie', queryParameters: {'query': query});
+    return _jsonToMovie(response.data);
+  }
 }
