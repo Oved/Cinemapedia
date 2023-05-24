bba0aa3170912cfa02471b6dcbefad7ae5fc8709
Oved Rincon <ogrinconm@ufpso.edu.co>
2023-05-24 15:35:52 -0500

    Se realiza avance en la busqueda de películas, se implementa en buscador y se realiza la busqueda cuando el usuario escribe

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppbar extends StatelessWidget {
+import '../../delegates/search_movie_delegate.dart';
+import '../../providers/movies/movies_repository_provider.dart';
+
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
     return SafeArea(
@@ -27,7 +31,14 @@ class CustomAppbar extends StatelessWidget {
                 ),
                 const Spacer(),
                 IconButton(
-                    onPressed: () {}, icon: const Icon(Icons.search_outlined))
+                    onPressed: () {
+                      final movieRepository = ref.read(movieRepositoryProvider);
+                      showSearch(
+                          context: context,
+                          delegate: SearchMovieDelegate(
+                              searchMovies: movieRepository.searchMovies));
+                    },
+                    icon: const Icon(Icons.search_outlined))
               ]),
             )));
   }
